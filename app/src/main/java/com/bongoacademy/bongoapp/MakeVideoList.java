2633d251f1fe794667bd5ebc4f20f5b53a75767e
nurnabimiah <diunayonahmed@gmail.com>
2023-02-17 08:26:34 +0600

    funny videos source code

---
+++ app/src/main/java/com/bongoacademy/bongoapp/MakeVideoList.java
@@ -0,0 +1,110 @@
+
+package com.bongoacademy.bongoapp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MakeVideoList {
+
+	public static ArrayList< ArrayList<HashMap<String,String>> > rootArrayList;
+	public static ArrayList< HashMap<String, String> > catArrayList;
+	public static ArrayList< HashMap<String, String> > videoArrayList;
+	public static HashMap<String, String> hashMap;
+
+
+	//--------------------------------------------------------------------------------------------
+	//===============================Some automation by Juba
+	public static void addVideoItem(String video_id, String title, String desciption){
+		hashMap = new HashMap<>();
+		hashMap.put("vdo_id", video_id);
+		hashMap.put("vdo_title", title);
+		hashMap.put("vdo_desciption", desciption);
+		videoArrayList.add(hashMap);
+	}
+	//========================================================================
+	//===============================Some automation by Juba
+	public static void createPlayList(String category_name, Integer drawable){
+		rootArrayList.add(videoArrayList);
+		hashMap = new HashMap<>();
+		hashMap.put("category_name", category_name);
+		hashMap.put("img", String.valueOf(drawable));
+		catArrayList.add(hashMap);
+		videoArrayList = new ArrayList<>();
+	}
+	//========================================================================
+
+
+
+
+
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//----------------------------------------------------
+	public static void createMyAlbums(){
+
+		rootArrayList = new ArrayList();
+		catArrayList = new ArrayList<>();
+		videoArrayList = new ArrayList<>();
+
+		//==========================================================================
+		addVideoItem("GgaxcNmm4UU", "Ei Mom Jochonay | Madhubanti Mukherjee", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("qPna1ZlRxTo", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("hegsoEcow8Q", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("kLHCDZeTVJk", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		createPlayList("Funny Dog", R.drawable.category_1);
+		//==========================================================================
+
+		//==========================================================================
+		addVideoItem("GgaxcNmm4UU", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("qPna1ZlRxTo", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("GgaxcNmm4UU", "Ei Mom Jochonay | Madhubanti Mukherjee", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("qPna1ZlRxTo", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		createPlayList("Funny Cat", R.drawable.category_2);
+		//==========================================================================
+
+
+		//==========================================================================
+		addVideoItem("GgaxcNmm4UU", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("qPna1ZlRxTo", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("GgaxcNmm4UU", "Ei Mom Jochonay | Madhubanti Mukherjee", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("qPna1ZlRxTo", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		createPlayList("Funny Baby", R.drawable.category_3);
+		//==========================================================================
+
+
+		//==========================================================================
+		addVideoItem("GgaxcNmm4UU", "Ei Mom Jochonay | Madhubanti Mukherjee", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("qPna1ZlRxTo", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("GgaxcNmm4UU", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("qPna1ZlRxTo", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		createPlayList("Funny Memes", R.drawable.category_4);
+		//==========================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+
+
+
+
+}
+
